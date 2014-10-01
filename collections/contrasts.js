02f1ed91b69663cd4af9e0c0224e8a621c4019f2
catalinabustam <catalinabustam@gmail.com>
2014-10-01 06:51:15 -0500

    con inventario por farmacioa y sede

---
+++ collections/contrasts.js
@@ -0,0 +1,56 @@
+ Contrasts = new Meteor.Collection('contrasts');
+
+Contrasts.allow({
+    update: ownsDocument,
+    remove: ownsDocument
+});
+	
+Meteor.methods({
+	contrast: function(contrastAttributes) {
+	    var user = Meteor.user();
+	    var order = Orders.findOne({accession:contrastAttributes.orderAcc});
+	    // ensure the user is logged in
+	    if (!user)
+	      throw new Meteor.Error(401, "Necesita hacer login para ingresar contraste");
+
+	    if (!contrastAttributes.dosis)
+		throw new Meteor.Error(422, 'Ingrese el número de la dosis');
+		
+	    //if (!contrastAttributes.ampolla)
+		//throw new Meteor.Error(422, 'Ingrese el número de la ampolla');
+		
+		if (!contrastAttributes.orderAcc)
+		throw new Meteor.Error(422, 'Debe ingresar el contraste a una orden');
+
+		var contrast = _.extend(_.pick(contrastAttributes, 'orderAcc', 'nombre','tipoampolla','dosis','donacion','donacionentidad','sede','modalidad'), {   
+			  userId: user._id,
+		      author: user.username,
+		      submitted: new Date()
+		  });
+		  
+		  var countn=contrast.countName
+		  var update={}
+		  update[countn]=1
+		  
+		  if (contrast.tipoampolla=='nueva'){
+		 invname=contrast.sede+ "_" +contrast.nombre
+		 inventario_his=Inventarios.findOne({type:'historial'})
+		 console.log(inventario_his)
+		 updcont={}
+		 updcont[invname]=-1
+		 Inventarios.update(inventario_his._id, {$inc: updcont})
+		}
+		
+		//Counters.update(contrast.counterId, {$inc: update});
+	    //conter=Counters.findOne(contrast.counterId)
+	    //countv=conter[countn]
+			// create the contrast, save the id
+		//contrast.counterNumber=countv
+		contrast._id = Contrasts.insert(contrast);
+			
+	
+		  // return countv
+	   }
+   });
+	   
+	
